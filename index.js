06747949e043a2a2b6c0901074ffc7141ba79310
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-11-08 17:17:10 +0900

    Finished sample echo service

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 exports.subscribe = (req, res) => {
-  // let message = req.query.message || req.body.message || 'commit test';
-  res.status(200).send('aaaaaaaaaa');
+  if (req.get('content-type') !== 'application/csp-report') {
+    res.status(400).send({
+      message: 'Invalid content type',
+    });
+  }
+
+  req.body = JSON.parse(req.body);
+  let userAgent = req.get('user-agent');
+
+  // TODO: Stores into BigQuery
+
+  res.status(200).send({
+    message: 'ok',
+    report: req.body,
+    ua: userAgent,
+  });
 }
 
